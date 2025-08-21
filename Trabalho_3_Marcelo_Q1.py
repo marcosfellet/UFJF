dd450963df519d6a3175fb03a8c05e0ea9bf34f6
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-08-21 01:07:51 -0300

    Update Trabalho_3_Marcelo_Q1.py
    Consertando o código

--- Trabalho_3_Marcelo_Q1.py
+++ Trabalho_3_Marcelo_Q1.py
@@ -1,14 +1,16 @@
 import numpy as np
 import plotly.graph_objects as go
-# Variaveis conhecidas 
+# Variaveis conhecidas
+ 
 Rs = 10e3 # resistencia de fonte
 R1 = 51e3  # resistencia de base 1
 R2 = 5.5*1000 # resistencia de base 2
 Rf = [160e3, 82e3 ,47e3 ] # resistencia de realimentacao
 RC = 10e3 # resistencia de coletor
 RE = 0.5e3  # resistencia de emissor
-re = 1.5 # resistencia do modelo T
-
+r_pi = 4.570e3 # resistencia do modelo pi 
+I_C = 5.47e-6 # corrente C.C. de coletor 
+VT = 25e-3 # tensao de limiar
 
 # Configuracao do Grafico
 # Lista de estilos de linha e cores
@@ -24,7 +26,8 @@ for i, rf_val in enumerate(Rf):
     beta = -1 / rf_val
     lista_ganho = []
     for beta_tbj in np.logspace(0, 4, num=1000):
-        A = -beta_tbj * ((1/RC + 1/rf_val)**(-1)) * ((((1/R1 + 1/Rs + 1/R2 + 1/rf_val)**(-1)) + (beta_tbj + 1)*(RE + re)) / ((beta_tbj + 1)*(RE + re) ))
+        gm = beta_tbj *(I_C/VT)
+        A = -gm*(((1/RC)+(1/rf_val))**(-1))*(((1/Rs)+(1/R2)+(1/R1)+(1/rf_val)+(1/r_pi))**(-1))
         Af = A / (1 + A*beta)
         Af_v =  Af / Rs
         lista_ganho.append(np.abs(Af_v))
@@ -95,14 +98,16 @@ fig.write_image("grafico_ganho.png")
 
     
 # Para comparar o valor de Af_v teorico com o obtido pela simulacao  
-beta_tbj = 100
-rf_val = 82e3
+beta_tbj = 200
+rf_val = 47e3
+gm = beta_tbj *(I_C/VT)
 beta = -1/rf_val
-A = -beta_tbj * ((1/RC + 1/rf_val)**(-1)) * ((((1/R1 + 1/Rs + 1/R2 + 1/rf_val)**(-1)) + (beta_tbj + 1)*(RE + re)) / ((beta_tbj + 1)*(RE + re) ))
+A = -gm*(((1/RC)+(1/rf_val))**(-1))*((1/Rs)+(1/R2)+(1/R1)+(1/rf_val)+(1/r_pi))**(-1)
 Af = A / (1 + A*beta)
 Af_v =  Af / Rs      
-print(Af_v)
-    
-
-
+print("/////////////////////////////////////////////////"),
+print(Af_v),
+print("/////////////////////////////////////////////////"),
+print(A),
+print(1+beta*A),
 
