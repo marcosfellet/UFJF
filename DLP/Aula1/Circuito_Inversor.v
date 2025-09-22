8e105ba850c36bf09444fca8e9a1a4945226aadf
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-09-22 10:06:18 -0300

    Add files via upload

---
+++ DLP/Aula1/Circuito_Inversor.v
@@ -0,0 +1,16 @@
+// Module e como se fosse uma classe que define um circuito
+module Circuito_Inversor //Comentario /*Comentario*/
+// Definicao de entradas e saidas:
+(
+	input in, 
+	output out
+);
+
+assign out = ~in; // o bit out recebe a saida de um circuito depois do igual (~in)
+
+endmodule
+
+
+
+
+
