fa3ce8f888fd5ff51ffa2149f64ca63d4dbc4b4d
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-09-24 16:09:41 -0300

    Create ponteiros.cpp

---
+++ Aula3/Nota_Guiherme/ponteiros.cpp
@@ -0,0 +1,5 @@
+/* otimiza o jeito de manipular variaveis locais em outras funcoes, pois seu tamanho e sempre o valor do barramento da memoria, se a memoria for de 32 bits, os ponteiros terao 
+32 bits, se for de 64 bits, os ponteiros terao 64 bits
+
+Exemplo: e possivel manipular uma variavel de 4 bytes com uma variave de 32/64 bits. O tempo de deslocamento de transmissao de dados e poupado enormemente
+*/
