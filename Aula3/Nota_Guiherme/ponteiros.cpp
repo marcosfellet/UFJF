a56efdb19d979ce44f5119ccd8ea98e55f7a8eb5
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-09-24 16:14:13 -0300

    Update ponteiros.cpp

--- Aula3/Nota_Guiherme/ponteiros.cpp
+++ Aula3/Nota_Guiherme/ponteiros.cpp
@@ -4,5 +4,10 @@ Otimiza o jeito de manipular variaveis locais em outras funcoes, pois seu tamanh
 
 Exemplo: e possivel manipular uma variavel de 4 bytes com uma variave de 32/64 bits. O tempo de deslocamento de transmissao de dados e poupado enormemente
 
-Vetores sao ponteiros, os quais podem manipular os valores em suas posicoes
+Vetores sao ponteiros constantes, os quais podem manipular os valores em suas posicoes
+
 */
+
+int vet[] = {1,2,3,4,5};
+*vet += 1; // incrementa o valor de vet[0]
+*(vet+1) *= 3; // multiplica o valor de vet[1] por 3   
