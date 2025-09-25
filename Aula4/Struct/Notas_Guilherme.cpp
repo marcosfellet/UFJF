3c0e1ebe1547bc4d1f3bdbb5939dbc25bbe1f293
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-09-25 15:27:17 -0300

    Update Notas_Guilherme.cpp

--- Aula4/Struct/Notas_Guilherme.cpp
+++ Aula4/Struct/Notas_Guilherme.cpp
@@ -2,7 +2,7 @@
 OBS: variaveis e um espaco de memoria nomeado
 Funcao: Agrupar variaveis de diferentes tipos
         Cria novos tipos de dados/variaveis, exemplo: cria variavel do tipo pessoa (nome, idade, altura, nacionalidade, etc)
-ponteiro se acessa da seguinte forma: ->
+ponteiro se acessa da seguinte forma: -> exemplo: pjoao->nome
 
 
 */
