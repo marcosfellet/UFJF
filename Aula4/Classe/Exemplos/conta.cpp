d00d7e4c174712909953aee3fda42ece2ad2e4b6
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-09-25 16:04:53 -0300

    Update conta.cpp

--- Aula4/Classe/Exemplos/conta.cpp
+++ Aula4/Classe/Exemplos/conta.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 void Conta::exibeDados()
 {
-	cout << "Titular: " << this->titular << endl;
-	cout << "Numero: " << this->numero << endl;
+	cout << "Titular: " << this->titular << endl; // this-> : ponteiro que aponta para o objeto corrente
+	cout << "Numero: " << this->numero << endl;  // Objeto corrente: o objeto que invocou o "exibeDados" (c1 ou c2)
 }
