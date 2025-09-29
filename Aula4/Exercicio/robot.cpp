734ba67bedc8bf3732e19608b9d5469037400bfd
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-09-29 10:21:35 -0300

    robot.cpp arrumada

--- Aula4/Exercicio/robot.cpp
+++ Aula4/Exercicio/robot.cpp
@@ -2,25 +2,21 @@
 #include <iostream>
 using namespace std;
 
-/*void Conta::exibeDados()
-{
-	cout << "Titular: " << this->titular << endl; // this-> : ponteiro que aponta para o objeto corrente
-	cout << "Numero: " << this->numero << endl;  // Objeto corrente: o objeto que invocou o "exibeDados" (c1 ou c2)
-}*/
-
 void Robot::showPos() //Exibe a posicao x e y do robo 
 {
-    cout << "Posição em x: " << this->robo.pos(1) << endl;
-    cout << "Posição em y: " << this->robo.pos(2) << endl;
+    cout << "Robo: " << this->robo.id << endl;
+    cout << "Posicao em x: " << this->robo.pos[0] << endl;
+    cout << "Posicao em y: " << this->robo.pos[1] << endl;
 } 
 void Robot::move(float t) // move o robô com as velocidades x e y pelo tempo determinado
 {
-    robo.pos(1) = t*robo.vel(1);
-    robo.pos(2) = t*robo.vel(2);
+    robo.pos[0] = t*robo.vel[0];
+    robo.pos[1] = t*robo.vel[1];
 }
 
-void Robot::changeSpeed(float a, float b)
+void Robot::changeSpeed(float a, float b) // muda a velocidade com que o robo se locomove 
 {
-    robo.vel(1) *= t/a;
-    robo.vel(2) *= t^2/b;
+    robo.vel[0] *= b*1.57/a;
+    robo.vel[1] *= (a*a)/b;
 }
+
