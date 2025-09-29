27f52e22f98acd89d52aa92b3f3cf8b3b6333b19
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-09-29 10:20:20 -0300

    main.cpp arrumada

--- Aula4/Exercicio/main.cpp
+++ Aula4/Exercicio/main.cpp
@@ -1,3 +1,9 @@
+/*******************************************************************************************************************************************
+		Exercicio utilizando Struct e Class 
+o intuito e simular, de maneira muito simplificada, o movimento de 2 robos. Dessa forma, a variavel "robo" e criada com o tipo definido como 
+"Parametros". Ela e utilizada pela classe "Robot", a qual possiu as funcoes necessarias para manipular as informacoes fornecidas pelas 
+variaveis "robo".
+********************************************************************************************************************************************/
 #include "robot.h"
 
 using namespace std;
@@ -8,17 +14,27 @@ int main()
 	float a = 0.47, b = 10.63; // parametros para mudar a velocidade
 	float t = 5; // tempo em segundos
 	// Robo 1
-	r1.robo = {0,0}; // inicialização das posições
-	r1.robo = {10,3}; // inicialização das velocidades
-	r1.showPos();
+	r1.robo.id = 1; // identificacao dos robos
+	// inicialização das posições
+	r1.robo.pos[0] = 0;
+	r1.robo.pos[1] = 0;
+	// inicialização das velocidades
+	r1.robo.vel[0] = 10;
+	r1.robo.vel[1] = 3;
 	r1.move(t);
 	r1.changeSpeed(a,b);
+	r1.showPos();
 	// Robo 2
-	r2.robo = {0,0}; // inicialização das posições
-	r2.robo = {6,12}; // inicialização das velocidades
-	r2.showPos();
+	r2.robo.id = 2; // identificacao dos robos
+	// inicialização das posições
+	r2.robo.pos[0] = 0;
+	r2.robo.pos[1] = 0;
+	// inicialização das velocidades
+	r2.robo.vel[0] = 50;
+	r2.robo.vel[1] = 0.42;
 	r2.move(t);
 	r2.changeSpeed(a, b);
+	r2.showPos();
 
 
 }
